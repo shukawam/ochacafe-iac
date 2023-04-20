6d16e401f0bfcc4436d3033fca825669090cb2ed
shukawam <shukawam@gmail.com>
2023-04-21 04:17:57 +0900

    Add live sample

---
+++ terraform/live/oke-tutorial/variables.tf
@@ -0,0 +1,26 @@
+#####
+# Commons
+variable "compartment_ocid" {
+}
+
+variable "region" {
+}
+
+#####
+# Virtual Cloud Network
+variable "vcn_display_name" {
+}
+
+#####
+# Oracle Container Engine for Kubernetes
+variable "cluster_name" {
+}
+
+variable "node_pool_instance_number" {
+}
+
+variable "node_pool_node_shape_config_memory_in_gbs" {
+}
+
+variable "node_pool_node_shape_config_ocpus" {
+}
