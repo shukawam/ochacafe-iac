537d3165a537af69497c04ffb3eba5036617bf3b
shukawam <shukawam@gmail.com>
2023-05-24 17:45:28 +0900

    Fix compartment_ocid

--- test/object-storage-first-step_test.go
+++ test/object-storage-first-step_test.go
@@ -1,7 +1,6 @@
 package test
 
 import (
-	"os"
 	"testing"
 
 	"github.com/gruntwork-io/terratest/modules/terraform"
@@ -14,7 +13,6 @@ func TestObjectStorageFirstStep(t *testing.T) {
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
 		TerraformDir: "../terraform/examples/object-storage-first-step",
 		Vars: map[string]interface{}{
-			"compartment_ocid":          os.Getenv("COMPARTMENT_ID"),
 			"region":                    "ap-tokyo-1",
 			"objectstorage_namespace":   "orasejapan",
 			"objectstorage_bucket_name": "test-bucket",
