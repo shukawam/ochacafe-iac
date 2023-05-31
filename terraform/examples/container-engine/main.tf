c7d97351a2fd7abd86cd8c1a3654acdbb99172f3
shukawam <shukawam@gmail.com>
2023-05-31 20:18:14 +0900

    Fixed oke example

--- terraform/examples/container-engine/main.tf
+++ terraform/examples/container-engine/main.tf
@@ -1,7 +1,12 @@
 module "shukawam_containerengine_cluster" {
   source                                    = "../../modules/container-engine"
-  compartment_ocid                          = var.compartment_ocid
   region                                    = var.region
+  tenancy_ocid                              = var.tenancy_ocid
+  user_ocid                                 = var.user_ocid
+  private_key_path                          = var.private_key_path
+  private_key                               = var.private_key
+  fingerprint                               = var.fingerprint
+  compartment_ocid                          = var.compartment_ocid
   vcn_id                                    = var.vcn_id
   lb_subnet_id                              = var.lb_subnet_id
   node_pool_subnet_id                       = var.node_pool_subnet_id
--- terraform/examples/container-engine/variables.tf
+++ terraform/examples/container-engine/variables.tf
@@ -1,29 +1,65 @@
-variable "compartment_ocid" {
+#####
+# Provider
+variable "region" {
+  description = "OCI Region Identifier. (e.g. ap-tokyo-1, ...)"
 }
 
-variable "region" {
+variable "tenancy_ocid" {
+  description = "OCID of tenancy."
+}
+
+variable "user_ocid" {
+  description = "OCID of user."
+}
+
+variable "private_key_path" {
+  default     = null
+  description = "File path of private key."
+}
+
+variable "private_key" {
+  default     = null
+  description = "Content of private key."
+}
+
+variable "fingerprint" {
+  description = "Fingerprint."
+}
+
+variable "compartment_ocid" {
+  description = "OCID of compartment."
+}
+
+#####
+# Container Engine
+variable "cluster_name" {
+  description = "Cluster name"
 }
 
 variable "vcn_id" {
-}
-
-variable "lb_subnet_id" {
-}
-
-variable "node_pool_subnet_id" {
+  description = "VCN OCID"
 }
 
 variable "k8s_api_endpoint_subnet_id" {
+  description = "Kubernetes API Endpoint Subnet OCID"
 }
 
-variable "cluster_name" {
+variable "lb_subnet_id" {
+  description = "Load Balancer Subnet OCID"
+}
+
+variable "node_pool_subnet_id" {
+  description = "Node Pool Subnet OCID"
 }
 
 variable "node_pool_instance_number" {
+  description = "Node Pool Instance Number"
 }
 
 variable "node_pool_node_shape_config_memory_in_gbs" {
+  description = "Instance Memory(GB)"
 }
 
 variable "node_pool_node_shape_config_ocpus" {
+  description = "Instance OCPUs"
 }
